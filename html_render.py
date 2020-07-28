6971cc3f20ef95b6c9737146e25be87a70d7543c
Chauncey Sun <chauncey.sun1982@gmail.com>
2020-07-28 11:11:43 -0700

    step 1

--- html_render.py
+++ html_render.py
@@ -11,14 +11,17 @@ class Element(object):
     tag = "html"
 
     def __init__(self, content=None):
-        self.contents = [content]
+        if content is None:
+            self.contents = []
+        else:
+            self.contents = [content]
 
     def append(self, new_content):
         self.contents.append(new_content)
 
     def render(self, out_file):
-        out_file.write("\n")
         out_file.write("<{}>".format(self.tag))
+        out_file.write("\n")
         for content in self.contents:
             out_file.write(content)
             out_file.write("\n")
